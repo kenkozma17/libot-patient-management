3eaa47f7b17d942efaa051cc63665f3d0b945c26
kenkozma17 <kenkozma17@gmail.com>
2024-10-08 20:28:43 +0800

    Started building out forms

--- resources/js/Pages/Patients/Partials/AddPatientForm.vue
+++ resources/js/Pages/Patients/Partials/AddPatientForm.vue
@@ -4,6 +4,9 @@
         <h1>Add New Patient Form</h1>
         <form action="#" class="bg-white rounded-md mt-2.5 md:px-[1.9rem] px-[1.25rem] md:py-[1.4rem] py-[1.125rem]">
             <h2 class="font-semibold">Personal Information</h2>
+            <div>
+
+            </div>
         </form>
     </div>
 </template>
